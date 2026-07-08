823e890c6a6de27d52b4d20cada0fb8729b14581
maan2003 <manmeetmann2003@gmail.com>
2026-07-08 12:53:51 +0000

    Use local cargo commands in selfci

--- .config/selfci/ci.sh
+++ .config/selfci/ci.sh
@@ -8,30 +8,17 @@ function job_cargo() {
   fi
 
   selfci step start "build"
-  if ! nix build -L .#ci.workspace; then
+  if ! cargo build --locked --workspace --all-targets; then
     selfci step fail
   fi
 
   selfci step start "clippy"
-  if ! nix build -L .#ci.clippy; then
+  if ! cargo clippy --locked --workspace --all-targets -- --allow deprecated --allow clippy::approx_constant; then
     selfci step fail
   fi
 
-  selfci step start "nextest"
-  if ! nix build -L .#ci.tests; then
-    selfci step fail
-  fi
-}
-
-function job_coverage() {
-  selfci step start "coverage tests"
-  if ! nix build -L .#ci.testsCcov; then
-    selfci step fail
-  fi
-
-  selfci step start "cargo-crap report"
-  if ! nix build -L .#ci.crapReport; then
-    >&2 echo "cargo-crap: failed - CRAP report generation failed"
+  selfci step start "test"
+  if ! cargo test --locked --workspace --lib --bins --tests; then
     selfci step fail
   fi
 }
@@ -39,13 +26,9 @@ function job_coverage() {
 case "$SELFCI_JOB_NAME" in
   main)
     selfci job start "cargo"
-    selfci job start "coverage"
     ;;
   cargo)
     job_cargo
-    ;;
-  coverage)
-    job_coverage
     ;;
   *)
     echo "Unknown job: $SELFCI_JOB_NAME"
