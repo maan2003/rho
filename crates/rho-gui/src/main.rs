6cf48badd3a7f129b93243ccae447e492f092d5a
maan2003 <manmeetmann2003@gmail.com>
2026-07-09 08:52:13 +0000

    Update GPUI color pipeline diagnostics

--- crates/rho-gui/src/main.rs
+++ crates/rho-gui/src/main.rs
@@ -68,7 +68,6 @@ fn main() {
 
 fn init_tracing() {
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let _ = tracing_log::LogTracer::init();
     if let Err(error) = tracing_subscriber::fmt()
         .with_env_filter(filter)
         .with_writer(std::io::stderr)
